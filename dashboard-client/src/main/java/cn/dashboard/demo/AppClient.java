dace3b11aace650a92fe1feb5b1b8b20d7f061a9
yichuancq <1012027293@qq.com>
2018-10-08 00:10:07 +0800

    add 资源文件

--- dashboard-client/src/main/java/cn/dashboard/demo/AppClient.java
+++ dashboard-client/src/main/java/cn/dashboard/demo/AppClient.java
@@ -1,6 +1,5 @@
 package cn.dashboard.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
